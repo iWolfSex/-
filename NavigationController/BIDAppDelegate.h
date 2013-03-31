33def20bc7b2961c3140c09b1cd435c269dd1ded
刘超 <liuchao@liuchaotekiMac.local>
2013-03-31 17:39:13 +0800

    Initial Commit

---
+++ NavigationController/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  NavigationController
+//
+//  Created by 刘超 on 13-3-31.
+//  Copyright (c) 2013年 刘超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NavigationController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavigationController
+//
+//  Created by 刘超 on 13-3-31.
+//  Copyright (c) 2013年 刘超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
